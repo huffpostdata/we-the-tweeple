d4cbd567e7934a8cdf190fbebce80ac215e1b053
Mike Stop Continues <mikestopcontinues@gmail.com>
2016-10-04 15:59:04 -0400

    async benchmark

--- scripts/render-pngs.js
+++ scripts/render-pngs.js
@@ -2,18 +2,30 @@
 
 const _ = require('lodash');
 const fs = require('fs-extra');
+const async = require('async');
 const Database = require('../app/Database.js');
 
 // config
 
 const db = new Database();
 
-// benchmark
+// sync benchmark - 26m
 
-console.time('10k');
+console.time('10k-sync');
 
 db.biggestTokens.forEach((token) => {
   token.renderPng();
 });
 
-console.timeEnd('10k');
+console.timeEnd('10k-sync');
+
+// async benchmark - 26m
+
+console.time('10k-async');
+
+// async.forEach(db.biggestTokens, (token, next) => {
+//   token.renderPng();
+//   next();
+// }, () => {
+//   console.timeEnd('10k-async');
+// });
