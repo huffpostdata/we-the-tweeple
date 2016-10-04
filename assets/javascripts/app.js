b4bd02e5a8e6c2a955a32c3736e0084cc3284390
Adam Hooper <adam@adamhooper.com>
2016-10-04 17:35:50 -0400

    Venn venn venn venn venn (diagrams)

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -147,18 +147,122 @@ function format_int(n) {
     .replace(/(\d)(?=(\d{3})+$)/g, '$1,');
 }
 
+/**
+ * Returns a <svg> string with a Venn diagram sized according to the input.
+ *
+ * Paramters:
+ *   nPopulation: focus population (e.g., "we're looking at 200 people")
+ *   nClinton: number of Clinton followers
+ *   nTrump: number of Trump followers
+ *   nBoth: number of Clinton followers who are also Trump followers
+ */
+function renderVennSvg(nPopulation, nClinton, nTrump, nBoth) {
+  var rClinton = 50 * Math.sqrt(nClinton / nPopulation);
+  var rTrump = 50 * Math.sqrt(nTrump / nPopulation);
+
+  // Fail fast for botnet-y stuff
+  if (rClinton === 0 && rTrump === 0) {
+    return '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 100"></svg>';
+  } else if (rClinton === 0) {
+    return [
+      '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 100">',
+        '<circle class="trump" cx="100" cy="50" r="', rTrump, '"/>',
+      '</svg>'
+    ].join('');
+  } else if (rTrump === 0) {
+    return [
+      '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 100">',
+        '<circle class="clinton" cx="100" cy="50" r="', rClinton, '"/>',
+      '</svg>'
+    ].join('');
+  }
+
+  var aClinton = rClinton * rClinton * Math.PI;
+  var aTrump = rTrump * rTrump * Math.PI;
+  var aBoth = Math.min(aTrump, aClinton, Math.max(aClinton, aTrump) * nBoth / Math.max(nClinton, nTrump));
+
+  if (aBoth === aClinton) {
+    return [
+      '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 100">',
+        '<circle class="clinton" cx="100" cy="50" r="', rClinton, '"/>',
+        '<circle class="trump" cx="100" cy="50" r="', rTrump, '"/>',
+      '</svg>'
+    ].join('');
+  } else if (aBoth === aTrump) {
+    return [
+      '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 100">',
+        '<circle class="trump" cx="100" cy="50" r="', rTrump, '"/>',
+        '<circle class="clinton" cx="100" cy="50" r="', rClinton, '"/>',
+      '</svg>'
+    ].join('');
+  }
+
+  // http://mathworld.wolfram.com/Circle-CircleIntersection.html
+  function dToArea(d) {
+    var p1 = rClinton * rClinton * Math.acos((d * d + rClinton * rClinton - rTrump * rTrump) / (2 * d * rClinton));
+    var p2 = rTrump * rTrump * Math.acos((d * d + rTrump * rTrump - rClinton * rClinton) / (2 * d * rTrump));
+
+    var inRoot = 1
+      * (-d + rClinton + rTrump)
+      * (+d + rClinton - rTrump)
+      * (+d - rClinton + rTrump)
+      * (+d + rClinton + rTrump);
+
+    return p1 + p2 - 1 / 2 * Math.sqrt(inRoot);
+  }
+
+  function dToY(d) {
+    var toRoot = 1
+      * (-d + rClinton - rTrump)
+      * (-d - rClinton + rTrump)
+      * (-d + rClinton + rTrump)
+      * (+d + rClinton + rTrump);
+    return 1 / 2 / d * Math.sqrt(toRoot);
+  }
+
+  function solveForD(a) {
+    var Tolerance2 = 0.00001;
+    var MaxIter = 10000;
+
+    var minD = 0;
+    var maxD = rClinton + rTrump;
+
+    for (var i = 0; i < MaxIter; i++) {
+      var tryD = (minD + maxD) / 2;
+      var err = dToArea(tryD) - a;
+      if (err < Tolerance2 && err > -Tolerance2) return tryD;
+      if (err < 0) {
+        // area we found is smaller than what we want. Smaller d => larger area
+        maxD = tryD;
+      } else {
+        // Our area is too big. Move circles apart. Smaller D => larger area
+        minD = tryD;
+      }
+    }
+
+    return (minD + maxD) / 2;
+  }
+
+  var d = solveForD(aBoth);
+  var xTrump = (d * d - rClinton * rClinton + rTrump * rTrump) / (2 * d);
+  var xClinton = d - xTrump;
+  var yArc = dToY(d);
+
+  return [
+    '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 100">',
+      '<circle class="clinton" cx="', (100 - xClinton), '" cy="50" r="', rClinton, '"/>',
+      '<circle class="trump" cx="', (100 + xTrump), '" cy="50" r="', rTrump, '"/>',
+      '<path class="both" d="',
+        'M100,', (50 + yArc),
+        'A', rTrump, ',', rTrump, ' 0 0,1 100,', (50 - yArc),
+        'A', rClinton, ',', rClinton, ' 0 0,1 100,', (50 + yArc),
+        'Z"/>',
+    '</svg>'
+  ].join('');
+}
+
 function format_group_as_diagram(group, max_value) {
-  var half_mid = group.nBoth / 2;
-  var left_value = group.nClinton - half_mid;
-  var right_value = group.nTrump - half_mid;
-  var full_width = max_value * 2;
-
-  var left1 = .5 - (left_value / full_width);
-  var width1 = group.nClinton / full_width;
-  var width2 = group.nTrump / full_width;
-  var left2 = .5 + (right_value / full_width) - width2;
-
-  return '<div class="log-bars"><div class="log-bar log-bar-left" style="left: ' + (left1 * 100) + '%; width: ' + (width1 * 100) + '%"></div><div class="log-bar log-bar-right" style="left: ' + (left2 * 100) + '%; width: ' + (width2 * 100) + '%"></div><div class="log-bar log-bar-both" style="left: ' + (left2 * 100) + '%; width: ' + (50 * group.nBoth / max_value) + '%;"></div></div>';
+  return renderVennSvg(max_value, group.nClinton, group.nTrump, group.nBoth);
 }
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -180,7 +284,7 @@ document.addEventListener('DOMContentLoaded', function() {
         results_el.innerHTML = 'No matches found';
         originals_el.innerHTML = '';
       } else {
-        var max_count = matches.reduce(function(s, m) { return Math.max(s, m.group.nClinton - m.group.nBoth / 2, m.group.nTrump - m.group.nBoth / 2); }, 0);
+        var max_count = matches.reduce(function(s, m) { return Math.max(s, m.group.n); }, 0);
 
         results_el.innerHTML = '<table><thead><tr><th>Word</th><th class="left" colspan="2">Clinton</th><th class="right" colspan="2">Trump</th></tr></thead><tbody>'
           + matches.map(function(m) {
