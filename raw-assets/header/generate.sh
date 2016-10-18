f2d8de15a97364e1fcad9ba00e30984c65048a14
Adam Hooper <adam@adamhooper.com>
2016-10-18 17:06:44 -0400

    JPEG quality -80. Saves lots of kb.

--- raw-assets/header/generate.sh
+++ raw-assets/header/generate.sh
@@ -4,5 +4,5 @@ IN="$(dirname "$0")"
 OUT="$IN"/../../assets/images
 
 set -x
-convert "$IN"/desktop.png -quality 85 "$OUT"/header-desktop.jpg
-convert "$IN"/mobile.png -quality 85 "$OUT"/header-mobile.jpg
+convert "$IN"/desktop.png -quality 80 "$OUT"/header-desktop.jpg
+convert "$IN"/mobile.png -quality 80 "$OUT"/header-mobile.jpg
