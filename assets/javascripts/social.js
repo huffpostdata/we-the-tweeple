520c823386ede4210c6677735010b3b69e61bb17
Adam Hooper <adam@adamhooper.com>
2016-10-19 16:45:43 -0400

    Set target=_blank when tweeting

--- assets/javascripts/social.js
+++ assets/javascripts/social.js
@@ -77,6 +77,7 @@
 
       var a = document.createElement('a');
       a.setAttribute('href', href);
+      a.setAttribute('target', '_blank');
       twttr.ready(function() { a.click(); });
     }
   });
